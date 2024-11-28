8a19d6dc80f317d06514466301d2b0c8f38d563f
Anne Castrillon <anne.castrillon@gmail.com>
2024-11-28 13:50:15 -0800

    minor changes

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -47,7 +47,7 @@ fn main() {
   cpu.load_game(bytes, Some(filepath.to_string()));
   cpu.load_bios(fs::read("../gba_bios.bin").unwrap());
 
-  // cpu.skip_bios();
+  cpu.skip_bios();
 
   let sdl_context = sdl2::init().unwrap();
   let video_subsystem = sdl_context.video().unwrap();

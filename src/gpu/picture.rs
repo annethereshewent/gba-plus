5927c1c8070b03d65be1e93cff27a679e61b5bd3
Anne Castrillon <anne.castrillon@gmail.com>
2025-07-20 13:08:59 -0700

    change picture to use RGBA8

--- src/gpu/picture.rs
+++ src/gpu/picture.rs
@@ -10,16 +10,17 @@ pub struct Picture {
 impl Picture {
   pub fn set_pixel(&mut self, x: usize, y: usize, rgb: (u8, u8, u8)) {
     // let i: usize = x * 3 + y * SCREEN_WIDTH as usize * 3;
-    let i: usize = 3 * (x + y * SCREEN_WIDTH as usize);
+    let i: usize = 4 * (x + y * SCREEN_WIDTH as usize);
 
     self.data[i] = rgb.0;
     self.data[i+1] = rgb.1;
     self.data[i+2] = rgb.2;
+    self.data[i+3] = 0xff;
   }
 
   pub fn new() -> Self {
     Picture {
-      data: vec![0; (3 * SCREEN_WIDTH as u32 * SCREEN_HEIGHT as u32) as usize]
+      data: vec![0; (4 * SCREEN_WIDTH as u32 * SCREEN_HEIGHT as u32) as usize]
     }
   }
 }
